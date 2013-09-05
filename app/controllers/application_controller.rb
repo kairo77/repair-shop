d2db715626064da72e58354eb67762bdf60879be
Chris Barnes <cbarnes@6fusion.com>
2013-09-05 17:03:14 +0000

    Updated creds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => "Kairo77", :password => "tl9008"
+  http_basic_authenticate_with :name => "kairo77", :password => "tl9008"
 end
